9d5e173e8e65416f29d5251528724f1219d01076
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-17 14:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let counter = document.getElementById('counter');
 let incrementBtn = document.getElementById('incrementBtn');
-let body=document.getElementByTag('body')
+// let body=document.getElementByTag('body')
 function increment() {
 	let currentCount = parseInt(counter.innerText);
 	alert(`${currentCount}`)
