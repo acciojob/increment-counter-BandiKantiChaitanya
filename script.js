84a1e1ea886f3292ba11238f6d06aba4c8a8b90c
BandiKantiChaitanya <bandikantichaitanya2@gmail.com>
2025-02-17 14:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let counter = document.getElementById('counter');
 let incrementBtn = document.getElementById('incrementBtn');
 // let body=document.getElementByTag('body')
 function increment() {
-	let currentCount = parseInt(counter.innerText);
-	counter.innerText=currentCount+1
-	alert(`${counter.innerText}`)
+    let currentCount = parseInt(counter.innerText);
+    counter.innerText = currentCount + 1;
+    displayCount.innerText = `Current count: ${counter.innerText}`;
 }
+
+incrementBtn.addEventListener('click', increment);
